2a6d9a9c81df324a5cf0afed1106a3369683a8b5
liuguogen <630989951@qq.com>
2016-01-05 10:51:22 +0800

    初始化代码

---
+++ Conf/db.php
@@ -0,0 +1,12 @@
+<?php
+return array (
+  'DB_TYPE' => 'mysql',
+  'DB_HOST' => 'localhost',
+  'DB_PORT' => '3306',
+  'DB_NAME' => 'weimi',
+  'DB_USER' => 'weimi',
+  'DB_PWD' => 'weimi',
+  'DB_PREFIX' => 'tp_',
+  'agent_version' => 1,
+);
+?>
---
+++ Conf/version.php
@@ -0,0 +1,7 @@
+<?php
+return array (
+  'ver' => '16.6',
+  'release' => '20150813',
+  'vername' => '微米WM微信接口平台-加盟版',
+);
+?>
