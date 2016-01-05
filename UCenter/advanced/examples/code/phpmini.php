0b426924b616ec35b85d8134718550a285b8645f
liuguogen <liuguogen@shopex.cn>
2016-01-05 14:51:42 +0800

    初始化代码

---
+++ UCenter/advanced/examples/code/phpmini.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ PHP Mini MySQL Admin
+ (c) 2004-2014 Oleg Savchuk <osalabs@gmail.com> http://osalabs.com
+
+ Light standalone PHP script for quick and easy access MySQL databases.
+ http://phpminiadmin.sourceforge.net
+
+ Dual licensed: GPL v2 and MIT, see texts at http://opensource.org/licenses/
+*/
+
+?>
